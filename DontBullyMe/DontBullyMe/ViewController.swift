de584536cc43ccf019f2e0d1ec96d37d040f2830
CeceXX <hello@cesaredecal.com>
2015-09-27 17:10:16 +0200

    Added search bar and get location button

--- DontBullyMe/DontBullyMe/ViewController.swift
+++ DontBullyMe/DontBullyMe/ViewController.swift
@@ -17,10 +17,13 @@ class ViewController: UIViewController, UISearchBarDelegate, MKMapViewDelegate, 
     
     // User Interface: UI objects
     @IBOutlet weak var mapView: MKMapView!
+    var searchController: UISearchController!
+    var searchResultsTableViewController: UITableViewController!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        // Retrieving location
         mapView.delegate = self
         mapView.showsUserLocation = true
         locationManager.delegate = self
@@ -32,6 +35,19 @@ class ViewController: UIViewController, UISearchBarDelegate, MKMapViewDelegate, 
         locationManager.desiredAccuracy = kCLLocationAccuracyBest
         locationManager.distanceFilter = kCLDistanceFilterNone
         self.locationManager.startUpdatingLocation()
+        
+        // Search bar stuff
+        searchResultsTableViewController = UITableViewController()
+        searchResultsTableViewController.view.backgroundColor = UIColor.whiteColor().colorWithAlphaComponent(1.0)
+        searchController = UISearchController(searchResultsController: searchResultsTableViewController)
+        searchController.delegate = self
+        searchController.hidesNavigationBarDuringPresentation = false
+        searchController.searchBar.delegate = self
+        self.navigationItem.titleView = searchController.searchBar
+        searchController.searchBar.placeholder = "Search for places..."
+        
+        //searchResultsTableViewController.tableView.delegate = self
+        //searchResultsTableViewController.tableView.dataSource = self
     }
     
     func locationManager(manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
@@ -43,5 +59,33 @@ class ViewController: UIViewController, UISearchBarDelegate, MKMapViewDelegate, 
     
     func locationManager(manager: CLLocationManager, didFailWithError error: NSError) {
         print("Error!")
+    }
+    
+    
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
+
+extension ViewController: UISearchControllerDelegate
+{
+    func willPresentSearchController(searchController: UISearchController) {
+        //caculate frame here
+        let statusBarHeight = UIApplication.sharedApplication().statusBarFrame.height
+        let navigationBarFrame = navigationController!.navigationBar.frame
+        let tableViewY = navigationBarFrame.height + statusBarHeight
+        let tableViewHeight: CGFloat = 30
+        
+        searchResultsTableViewController.tableView.frame = CGRectMake(0, tableViewY, navigationBarFrame.width, tableViewHeight)
+        
+        
+    }
+    override func viewWillLayoutSubviews() {
+    }
+    func presentSearchController(searchController: UISearchController) {
+    }
+    func didPresentSearchController(searchController: UISearchController) {
     }
 }
