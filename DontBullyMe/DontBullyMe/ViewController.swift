a39286eee4b522f58de1e235fab8417511eb7ec7
JimboPower <edoardo.decal@gmail.com>
2015-09-27 13:58:53 +0200

    Added Map and navigation controller

--- DontBullyMe/DontBullyMe/ViewController.swift
+++ DontBullyMe/DontBullyMe/ViewController.swift
@@ -12,14 +12,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        print("Questo codice viene fatto andare una sola volta")
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
 
