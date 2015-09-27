67ee75445e01bbc9c2ea516080525e74e78cf791
JimboPower <edoardo.decal@gmail.com>
2015-09-27 15:37:23 +0200

    Added user location

--- DontBullyMe/DontBullyMe/ViewController.swift
+++ DontBullyMe/DontBullyMe/ViewController.swift
@@ -7,12 +7,41 @@
 //
 
 import UIKit
+import MapKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UISearchBarDelegate, MKMapViewDelegate, CLLocationManagerDelegate {
+    
+    // Objects
+    var locationManager: CLLocationManager = CLLocationManager() // objects that tracks of the user's location changes
+    var userLocation: CLLocation!
+    
+    // User Interface: UI objects
+    @IBOutlet weak var mapView: MKMapView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("Questo codice viene fatto andare una sola volta")
+        
+        mapView.delegate = self
+        mapView.showsUserLocation = true
+        locationManager.delegate = self
+        
+        if CLLocationManager.authorizationStatus() == .NotDetermined {
+            locationManager.requestWhenInUseAuthorization()
+        }
+        
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.distanceFilter = kCLDistanceFilterNone
+        self.locationManager.startUpdatingLocation()
+    }
+    
+    func locationManager(manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let newLocation = locations.last!
+        mapView.setCenterCoordinate(newLocation.coordinate, animated: true)
+        let viewRegion = MKCoordinateRegionMakeWithDistance(newLocation.coordinate, 500, 500)
+        mapView.setRegion(viewRegion, animated: true)
+    }
+    
+    func locationManager(manager: CLLocationManager, didFailWithError error: NSError) {
+        print("Error!")
     }
 }
-
